aa24a4be45719a52211df52785bfd69369ef984e
evgen <evgen@pochta.ru>
2024-11-11 17:36:42 +0500

    fdffsf

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,14 @@ public class Main {
     public void testMethod(){
         System.out.println("fds");
     }
+
+    public void testMethod111(){
+        System.out.println("fds");
+    }
+
+    public void testMethod11111(){
+        System.out.println("fds");
+    }
     public void testMethod1(){
         System.out.println("fds");
     }
